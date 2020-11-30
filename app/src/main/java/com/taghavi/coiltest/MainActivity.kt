fdb71773ee86f68f203959934d4a59fc548b6662
EhsanTgv <EhsanTgv@gmail.com>
2020-11-30 23:46:43 +0330

    Initial commit

---
+++ app/src/main/java/com/taghavi/coiltest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taghavi.coiltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
