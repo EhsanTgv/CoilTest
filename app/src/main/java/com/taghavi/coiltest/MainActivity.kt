cdc7f6b7485ab4354e4dd9307301a15510153efd
EhsanTgv <EhsanTgv@gmail.com>
2020-12-01 09:15:32 +0330

    load simple image

--- app/src/main/java/com/taghavi/coiltest/MainActivity.kt
+++ app/src/main/java/com/taghavi/coiltest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.taghavi.coiltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.api.load
+import com.taghavi.coiltest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.load("https://file.example.vn/images/file_example_JPG_100kB.jpg")
     }
 }
